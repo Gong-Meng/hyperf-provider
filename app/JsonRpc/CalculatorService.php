a97f759e5296943b118815fd009ac3e3a7b42967
gongmeng <18588264128@163.com>
2022-03-15 23:06:47 +0800

    [服务提供者] 中心处理

--- app/JsonRpc/CalculatorService.php
+++ app/JsonRpc/CalculatorService.php
@@ -5,7 +5,7 @@ namespace App\JsonRpc;
 use Hyperf\RpcServer\Annotation\RpcService;
 
 /**
- * @RpcService(name="CalculatorService", protocol="jsonrpc-http", server="jsonrpc-http")
+ * @RpcService(name="CalculatorService", protocol="jsonrpc-http", server="jsonrpc-http", publishTo="consul")
  */
 class CalculatorService implements CalculatorServiceInterface
 {
